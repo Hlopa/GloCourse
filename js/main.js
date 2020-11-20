a6df194d7736f1291fa46e6b56296fd54cdfada3
Olga Barishevskaya <bariolga@yandex.ru>
2020-11-20 21:48:03 +0300

    Сделала задание 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 let money = 500,
     income = 'стипендия',
-    addExpenses = 'учеба, проездной, коммуналка',
+    addExpenses = 'Учеба, Проездной, Коммуналка',
     deposit = true,
     mission = 7000,
-    period = 6;
+    period = 6,
+    budgetDay = money/30;
+  
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
+console.log(addExpenses.length);
 
+console.log(`Период равен ${period} месяцам`);
+console.log(`Цель заработать ${mission} рублей`);
+
+let lowAddExpenses = addExpenses.toLowerCase();
+let arrAddExpenses = lowAddExpenses.split(", ");
+console.log(lowAddExpenses);
+console.log(arrAddExpenses);
+
+ console.log(budgetDay);
